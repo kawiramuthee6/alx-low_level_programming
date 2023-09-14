78082557d2659f1a13f40d60d23cf2dd75097fc6
kawiramuthee6 <mutheesharon5@gmail.com>
2023-09-14 07:47:15 -0700

    0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * positive_or_negative-Determine sign of integer, prints appropriate message
+ *
+ * @i: The integer to be checked.
+ */
 
 void positive_or_negative(int i)
 {
-    if (i > 0)
-        printf("%d is positive\n", i);
-    else if (i == 0)
-        printf("%d is zero\n", i); 
-    else
-        printf("%d is negative\n", i);
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+{
+printf("%d is negative\n", i);
+}
 }
