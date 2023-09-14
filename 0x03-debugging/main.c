a972aa2711a3720c37c6fd484971801f64e46e60
kawiramuthee6 <mutheesharon5@gmail.com>
2023-09-14 12:25:34 -0700

    3

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,16 +1,31 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
-int main(void)
+ * convert_day - Convert a day and return a value.
+ * @month: The month of the date.
+ * @day: The day of the date.
+ *
+ * This function takes a month and day as input and performs a conversion.
+ * It returns a value calculated based on the input values.
+ *
+ * Return: The converted value.
+ */
+int convert_day(int month, int day)
 {
-	int i;
+	return (day_of_the_year);
+}
 
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
+/**
+ * print_remaining_days - Print the remaining days in a year.
+ * @month: The current month.
+ * @day: The current day.
+ * @year: The year.
+ *
+ * This function calculates and prints the remaining days in the year
+ * based on the current month, day, and year.
+ */
+void print_remaining_days(int month, int day, int year)
+{
+	printf("Remaining days: %d\n", remaining_days);
 }
