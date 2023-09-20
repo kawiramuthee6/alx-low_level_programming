c3060d1a50440f8e68199e802ebaa6053a43d074
kawiramuthee6 <mutheesharon5@gmail.com>
2023-09-20 11:26:17 -0700

    102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,20 +2,22 @@
 
 int main(void)
 {
-int n;
-int a[5];
-int *p;
+  int n;
+  int a[5];
+  int *p;
 
-a[2] = 1024;
-p = &n;
-
-/*
-* You are not allowed to use 'a' and not allowed to modify 'p'.
-* You can't access memory beyond what 'p' points to.
-* So, achieving the desired result is not possible with these constraints.
-*/
-
-/* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
-return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
