2a08652d803aa18ff0d264b654c31a27310b46bd
kawiramuthee6 <mutheesharon5@gmail.com>
2023-09-20 10:19:55 -0700

    102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
 
-/**
-* main - Entry point for demonstrating modification of an array element.
-*
-* Description:
-* This program modifies the value of an element in an array by using a pointer.
-* It assigns 98 to a[2] by manipulating the pointer p.
-*
-* Return: Always 0 (Success)
-*/
 int main(void)
 {
 int n;
@@ -18,10 +9,13 @@ int *p;
 a[2] = 1024;
 p = &n;
 
-/* Modify a[2] indirectly using the pointer p */
-*(p + 5) = 98;
-/* Print the modified value of a[2] */
-printf("a[2] = %d\n", a[2]);
+/*
+* Use printf to directly print the value of a[2] without using 'a'.
+* The format specifier %d is used to print an integer.
+*/
+printf("a[2] = %d\n", *(p + 2));
 
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
 return (0);
 }
