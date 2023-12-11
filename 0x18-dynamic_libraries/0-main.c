7d0d6ccca6fd948ba83f55ea82ed38d60c1a22e7
kawiramuthee6 <mutheesharon5@gmail.com>
2023-12-11 15:06:56 -0800

    task 0

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-    printf("%d\n", _strlen("My Dyn Lib"));
-    return (EXIT_SUCCESS);
+printf("%d\n", _strlen("My Dyn Lib"));
+return (EXIT_SUCCESS);
 }
 julien@ubuntu:~/0x18. Dynamic libraries$ gcc -Wall -pedantic -Werror -Wextra -L. 0-main.c -ldynamic -o len
-julien@ubuntu:~/0x18. Dynamic libraries$ ldd len 
-    linux-vdso.so.1 =>  (0x00007fff5d1d2000)
-    libdynamic.so => not found
-    libc.so.6 => /lib/x86_64-linux-gnu/libc.so.6 (0x00007f74c6bb9000)
-    /lib64/ld-linux-x86-64.so.2 (0x0000556be5b82000)
+julien@ubuntu:~/0x18. Dynamic libraries$ ldd len
+linux-vdso.so.1 =>  (0x00007fff5d1d2000)
+libdynamic.so => not found
+libc.so.6 => /lib/x86_64-linux-gnu/libc.so.6 (0x00007f74c6bb9000)
+/lib64/ld-linux-x86-64.so.2 (0x0000556be5b82000)
